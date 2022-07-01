6b25249a94708e4ff7458b30b4c91efce993ca60
vixboss <vkrnt.lal@rediffmail.com>
2022-07-01 18:55:59 +0530

    Changes in discount stripe params

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import {Container, Row, Col} from 'react-bootstrap';
 
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from "../../components/sign-in/sign-in.component";
 import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'>
-        <SignIn/>
-        <SignUp/>
-    </div>
+    <Container className = "sign-in-and-sign-up-page">
+        <Row md={8}>
+            <Col>
+                <SignIn/>
+            </Col>
+            <Col>
+                <SignUp/>
+            </Col>
+        </Row>
+    </Container>
 )
 
 export default SignInAndSignUpPage;
